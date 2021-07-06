211bc686c94ed8053d560ec891f7266882945fdf
gizem <gizemates@iyte.edu.tr>
2021-07-06 17:54:05 +0200

    smooth joint space and task space cartesian control

--- src/send_and_receive.py
+++ src/send_and_receive.py
@@ -11,20 +11,54 @@ import time
 
 
 def initialize_ur_rtde():
+    global rtde_c, rtde_r
     rtde_c = rtde_control.RTDEControlInterface("172.31.1.144")
     rtde_r = rtde_receive.RTDEReceiveInterface("172.31.1.144")
     init_q = rtde_r.getActualQ()
-    return init_q
+    init_t = rtde_r.getActualTCPPose()
+    print("Initialized")
+    return init_q, init_t
+
+def custom_movej(joint_angles):
+    global rtde_c
+    new_joints = joint_angles
+    new_joints[5] = new_joints[5] + 0.2
+    rtde_c.moveJ(new_joints, 1.05, 1.4, True)
+    time.sleep(1.0)
+
+    new_joints[5] = new_joints[5] - 0.2
+    rtde_c.moveJ(new_joints, 1.05, 1.4, True)
+    time.sleep(1.0)
+    # Stop the movement before it reaches new_q
+    rtde_c.stopJ(0.5)
+
+
+def custom_movel(tcp_pose):
+    global rtde_c
+    new_target = tcp_pose
+    new_target[0] = new_target[0] + 0.4
+    rtde_c.moveL(new_target, 0.25, 0.5, True)
+    time.sleep(1.0)
+    rtde_c.stopL(0.5)
+
+    new_target[0] = new_target[0] - 0.4
+    rtde_c.moveL(new_target, 0.25, 0.5, True)
+    time.sleep(2.0)
+
+    # Stop the movement before it reaches target
+    rtde_c.stopL(0.5)
 
 
 def initialize_ros_node():
     # pub = rospy.Publisher('/current_pose', String, queue_size=10)
-    current_pose = initialize_ur_rtde()
+    joint_angles, tcp_pose = initialize_ur_rtde()
     rospy.init_node('send_and_receive', anonymous=False)
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        print(current_pose)
+        print(tcp_pose)
+        # custom_movej(joint_angles)
+        custom_movel(tcp_pose)
         rate.sleep()
 
 if __name__ == '__main__':
