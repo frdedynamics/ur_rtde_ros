056a4885bb1a582d2697dfa3d5fe26039529c59d
Gizem <gizemates@iyte.edu.tr>
2021-08-24 11:12:44 +0200

    make sure you send moveL wrt Base

--- src/imu_ur_rtde_test.py
+++ src/imu_ur_rtde_test.py
@@ -14,21 +14,26 @@ sys.path.insert(0, "/home/gizem/catkin_ws/src/my_human_pkg/src/Classes")
 import Kinematics_with_Quaternions_py3 as kinematic
 
 from rtde_control import RTDEControlInterface as RTDEControl
+import rtde_receive
 
 test_str = String()
 
 
 def main():
     rtde_c = RTDEControl("172.31.1.144", RTDEControl.FLAG_USE_EXT_UR_CAP)
+    rtde_r = rtde_receive.RTDEReceiveInterface("172.31.1.144")
     rospy.init_node('ur_rtde_test')
     pub = rospy.Publisher('/test_str', String, queue_size=1)
     rate = rospy.Rate(50)
 
     while not rospy.is_shutdown():
         try:
-            rtde_c.moveL([-0.143, -0.435, 0.20, -0.001, 3.12, 0.04], 0.5, 0.3)
+            # print(rtde_c.getActualJointPositionsHistory())
+            rtde_c.moveL([0.106, 0.715, -0.0, 2.616, -2.477, -1.510], 0.5, 0.3)
+            target = rtde_r.getActualTCPPose()
+            # rtde_c.moveJ([d2r(92.70), d2r(-121.12), d2r(-92.18), d2r(-174.31), d2r(-87.61), d2r(-91.58)], 0.5, 0.3)
             test_str.data = "Gizem"
-            print(test_str)
+            print(type(target))
             pub.publish(test_str)
             rate.sleep()
         except KeyboardInterrupt:
