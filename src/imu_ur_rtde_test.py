080d0348441c75dabe22f8455050533527cd90f5
Gizem <gizemates@iyte.edu.tr>
2021-08-24 11:46:45 +0200

    dummy imu is added

--- src/imu_ur_rtde_test.py
+++ src/imu_ur_rtde_test.py
@@ -6,17 +6,15 @@ import numpy
 from math import radians as d2r
 from math import degrees as r2d
 from geometry_msgs.msg import TwistStamped, Twist, Vector3, Quaternion, PoseStamped, Point
-from sensor_msgs.msg import Imu
 from std_msgs.msg import String
 
 import sys
 sys.path.insert(0, "/home/gizem/catkin_ws/src/my_human_pkg/src/Classes")
-import Kinematics_with_Quaternions_py3 as kinematic
+import Kinematics_with_Quaternions_py3 as kinematichuman_joint_imu
+from IMU_class_single import IMUsubscriber
 
 from rtde_control import RTDEControlInterface as RTDEControl
 import rtde_receive
-
-test_str = String()
 
 
 def main():
@@ -24,6 +22,11 @@ def main():
     rtde_r = rtde_receive.RTDEReceiveInterface("172.31.1.144")
     rospy.init_node('ur_rtde_test')
     pub = rospy.Publisher('/test_str', String, queue_size=1)
+
+    test_str = String()
+    myIMU = IMUsubscriber()
+    myIMU.init_subscribers_and_publishers()
+    
     rate = rospy.Rate(50)
 
     while not rospy.is_shutdown():
@@ -35,6 +38,7 @@ def main():
             test_str.data = "Gizem"
             print(type(target))
             pub.publish(test_str)
+            myIMU.update()
             rate.sleep()
         except KeyboardInterrupt:
             rtde_c.stopScript()
